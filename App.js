828bfbe0a3ab1cd643bd135f08bfb215ae3f10cf
Christian JW Klein <cklein010715@gmail.com>
2022-02-22 11:57:52 -0500

    first react native commit

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.headingText}>
+        Have you been a Miami Dolphins fan your whole life? {"\n"}You may be
+        entitled to financial compensation.
+        <Button
+          style={styles.button}
+          title="Click Here to Learn More"
+          color="darkgray"
+          onPress={() => Alert.alert("Stephen Ross is to blame")}
+        />
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "teal",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headingText: {
+    fontSize: 40,
+    color: "darkorange",
+    fontWeight: "900",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    color: "darkgray",
   },
 });
